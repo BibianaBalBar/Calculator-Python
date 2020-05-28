12b1f626429e129501bf50ab86f5f99e1be7ed89
Bibiana Barbosa <balbuenabibi@gmail.com>
2020-05-28 10:16:09 +0100

    func repeat and welcome

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 ## Perfoms basic math operations
 
 Runs in terminal
+
+    Welcome to Python Calculator
+
+
+        Please type in the math operation you would like to complete:
+        + for addition
+        - for subtraction
+        * for multiplication
+        / for division
+        ** for power
+        % for modulo
+        *
+    Enter your first number: 17
+    Enter your second number: 84
+    17 * 84 = 
+    1428
+
+    Do you want to keep calculating?
+    Y/N.
+    n
+    Thank you! See you later.
--- calculator.py
+++ calculator.py
@@ -1,3 +1,9 @@
+def welcome():
+    print('''
+Welcome to Python Calculator
+''')
+
+
 def calculate():
     operation = input('''
     Please type in the math operation you would like to complete:
@@ -5,6 +11,8 @@ def calculate():
     - for subtraction
     * for multiplication
     / for division
+    ** for power
+    % for modulo
     ''')
 
     number_1 = int(input('Enter your first number: '))
@@ -26,9 +34,34 @@ def calculate():
         print(f'{number_1} / {number_2} = ')
         print(number_1 / number_2)
 
+    elif operation == '**':
+        print(f'{number_1} ** {number_2} = ')
+        print(number_1 ** number_2)
+
+    elif operation == '%':
+        print(f'{number_1} % {number_2} = ')
+        print(number_1 % number_2)
+
     else:
-        print('You have not typed a valid operator, please run the program again.')
+        print('You have not typed a valid operator.')
 
 
-# Call calculate()
+def repeat():
+    calc_repeat = input('''
+Do you want to keep calculating?
+Y/N.
+''')
+
+    if calc_repeat.upper() == 'Y':
+        calculate()
+        repeat()
+    elif calc_repeat.upper() == 'N':
+        print('Thank you! See you later.')
+    else:
+        repeat()
+
+
+# Call funcs()
+welcome()
 calculate()
+repeat()
